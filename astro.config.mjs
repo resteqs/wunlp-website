889b8ed8961c3fd45042e35836d186db6d67d4ba
resteqs <antoni.mackowiak@protonmail.com>
2026-04-21 10:12:32 +0200

    Fix: add trailing slash to base config to properly resolve all internal paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://resteqs.github.io',
-  base: '/wunlp-website',
+  base: '/wunlp-website/',
   vite: {
     plugins: [tailwindcss()]
   }
